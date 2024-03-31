18d45d7b891e1ab2788b8bee2cc2d8a0daca0710
Lenny Wandeto <lenny.wandeto@student.moringaschool.com>
2024-03-31 15:55:02 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+
+function superbowlWin(record){
+    let gameWin = record.find(({result}) => result === "W")
+   if (gameWin){
+    return gameWin.year
+   } else{
+    return undefined
+   }
+    
+    
+}
+
